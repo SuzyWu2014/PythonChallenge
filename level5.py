991152f8b5bfd46d8a9c021fc037805243a6844d
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-05 18:17:40 -0700

    level 6 solution

--- level5.py
+++ level5.py
@@ -8,7 +8,9 @@ data = pickle.load(urlopen(url))
 for line in data:
 	print( "".join(elem[0]*elem[1] for elem in line ))
 
-
+# for line in data:
+#    print "".join(map(lambda pair: pair[0]*pair[1], line))
+   
 # next level: channel
 
 
---
+++ level6.py
@@ -0,0 +1,40 @@
+import zipfile, re, urllib
+
+o, number, file = [], "90052", "%s.txt"
+content = "Next nothing is (\d+)"
+
+zip_url = "http://www.pythonchallenge.com/pc/def/channel.zip"
+urllib.urlretrieve(zip_url,"channel.zip")
+
+zip_archive = zipfile.ZipFile("channel.zip")
+zipdict = {}
+
+for info in zip_archive.infolist():
+    zipdict[info.filename] = info.comment
+
+current_nothing = '90052'
+while True:
+    print zipdict[current_nothing + '.txt'],
+    page = zip_archive.read(current_nothing + '.txt')
+    try:
+        current_nothing = page.split('Next nothing is ')[1]
+    except IndexError:
+        break
+
+# zobj = StringIO()
+# zobj.write(urllib.urlopen("http://pythonchallenge.com/pc/def/channel.zip").read())
+# z = zipfile.ZipFile(zobj)
+
+# filenum = "90052"
+# lcomment = []
+
+# while True:
+#     if filenum.isdigit():
+#         filename = filenum + '.txt'
+#         lcomment.append(z.getinfo(filename).comment)
+#         info = z.read(filename)
+#         filenum = info.split(' ')[-1]
+#     else:
+#         break
+# z.close()
+# print ''.join(lcomment)
