e7b16da959698d7aad9436b577f96db102c522f3
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-06-12 01:28:11 -0700

    more style fix

--- level5.py
+++ level5.py
@@ -6,7 +6,7 @@ url = "http://www.pythonchallenge.com/pc/def/banner.p"
 data = pickle.load(urlopen(url))
 
 for line in data:
-	print("".join(elem[0] * elem[1] for elem in line))
+    print("".join(elem[0] * elem[1] for elem in line))
 
 # for line in data:
 #    print "".join(map(lambda pair: pair[0]*pair[1], line))
